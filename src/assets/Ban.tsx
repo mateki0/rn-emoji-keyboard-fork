4a55da262e50ee6c5257324f1358e18954d6452e
mateki0 <mateki0@interia.pl>
2023-06-14 11:42:01 +0200

    deleted unused imports

--- src/assets/Ban.tsx
+++ src/assets/Ban.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { Text } from 'react-native'
-import Svg, { FillProps, Path } from 'react-native-svg'
 
-export default ({ fill }: FillProps) => (
+
+export default () => (
   <Text style={{fontFamily:'icomoon'}}>{String.fromCodePoint(59648)}</Text>
   
 )
